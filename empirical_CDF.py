da491305c4dbe9bf7c19bf0659c07cd2d2bd7e16
Jeremie Coullon <jeremie.coullong@gmail.com>
2020-01-30 16:38:45 +0000

    more edits

--- empirical_CDF.py
+++ empirical_CDF.py
@@ -6,7 +6,7 @@ from PRT import run_PRT
 
 
 """
-Run PRT and plot the empirical CDF 
+Run PRT and plot the empirical CDF
 
 B: number of iterations of the PRT
 num_data: number of data points to generate
@@ -16,9 +16,6 @@ B = 200
 results_10 = run_PRT(num_data=10, B=B)
 array_samples = np.array([elem['posterior'] for elem in results_10])
 array_samples.sort()
-# plt.scatter(array_samples, np.arange(B)/B, s=4)
-# plt.plot(range(0, 11), np.arange(0, 1.1,0.1) , lw=1, c='r')
-
 
 plt.scatter(array_samples, np.arange(B)/B, s=4)
 plt.plot(range(0, 11), np.arange(0, 1.1,0.1) , lw=1, c='r')
