74ba99ccafa7d0d547b47b82251786702527bd9a
Jeremie Coullon <jeremie.coullong@gmail.com>
2020-01-31 08:59:33 +0000

    image scripts fix

--- PRT_compare_data_size.py
+++ PRT_compare_data_size.py
@@ -14,7 +14,7 @@ num_data: number of data points to generate. Try 2, 10, and 100
 
 
 B = 200
-results_2 = run_PRT(num_data=1, B=B)
+results_2 = run_PRT(num_data=2, B=B)
 results_10 = run_PRT(num_data=10, B=B)
 results_100 = run_PRT(num_data=100, B=B)
 
@@ -24,22 +24,22 @@ fig, ax = plt.subplots(3, figsize=(7,7))
 posterior_samples_2 = [elem['posterior'] for elem in results_2]
 prior_samples_2 = [elem['prior'] for elem in results_2]
 ax[0].scatter(posterior_samples_2, prior_samples_2, s=4)
-ax[0].set_xlabel("Posterior samples")
-ax[0].set_ylabel("Prior samples")
-ax[0].set_title("N=1", size=15)
+ax[0].set_xlabel("Posterior samples", size=12)
+ax[0].set_ylabel("Prior samples", size=12)
+ax[0].set_title("N=2", size=15)
 
 posterior_samples_10 = [elem['posterior'] for elem in results_10]
 prior_samples_10 = [elem['prior'] for elem in results_10]
 ax[1].scatter(posterior_samples_10, prior_samples_10, s=4)
-ax[1].set_xlabel("Posterior samples")
-ax[1].set_ylabel("Prior samples")
+ax[1].set_xlabel("Posterior samples", size=12)
+ax[1].set_ylabel("Prior samples", size=12)
 ax[1].set_title("N=10", size=15)
 
 posterior_samples_100 = [elem['posterior'] for elem in results_100]
 prior_samples_100 = [elem['prior'] for elem in results_100]
 ax[2].scatter(posterior_samples_100, prior_samples_100, s=4)
-ax[2].set_xlabel("Posterior samples")
-ax[2].set_ylabel("Prior samples")
+ax[2].set_xlabel("Posterior samples", size=12)
+ax[2].set_ylabel("Prior samples", size=12)
 ax[2].set_title("N=100", size=15)
 
 
--- empirical_CDF.py
+++ empirical_CDF.py
@@ -19,9 +19,9 @@ array_samples.sort()
 
 plt.scatter(array_samples, np.arange(B)/B, s=4)
 plt.plot(range(0, 11), np.arange(0, 1.1,0.1) , lw=1, c='r')
-plt.xlabel(r"$\theta$")
-plt.ylabel("empirical CDF")
-plt.title("Emprirical CDF", size=18)
+plt.xlabel(r"$\theta$", size=18)
+plt.ylabel("empirical CDF", size=18)
+plt.title("Empirical CDF", size=18)
 
 
 plt.tight_layout()
